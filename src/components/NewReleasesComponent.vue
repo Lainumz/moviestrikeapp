9604b41e67e55600f7648c0440a9625b4cf04772
Lainumz <sergilinuesa333@gmail.com>
2024-06-15 10:31:07 +0200

    estrenos

--- src/components/NewReleasesComponent.vue
+++ src/components/NewReleasesComponent.vue
@@ -3,17 +3,8 @@
     <h1>New Releases</h1>
     <div v-if="loading">Loading new releases...</div>
     <div v-else>
-      <div class="genre-select">
-        <label for="genre">Select Genre:</label>
-        <select v-model="selectedGenre" @change="filterMovies">
-          <option value="" selected>All</option>
-          <option v-for="genre in genres" :key="genre.id" :value="genre.id">
-            {{ genre.name }}
-          </option>
-        </select>
-      </div>
-      <div v-if="filteredMovies.length">
-        <div v-for="movie in paginatedMovies" :key="movie.id" class="movie">
+      <div v-if="newReleases.length">
+        <div v-for="movie in newReleases" :key="movie.id" class="movie">
           <router-link :to="{ name: 'movieDetail', params: { id: movie.id } }">
             <div class="tooltip">
               <img :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" :alt="movie.title" />
@@ -26,65 +17,21 @@
         <p>No new releases available.</p>
       </div>
     </div>
-    <div class="pagination">
-      <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
-      <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useMovieStore } from '@/store/movies'
-import { useGenreStore } from '@/store/genres'
 import type { Movie } from '@/types/movie'
 
 const movieStore = useMovieStore()
-const genreStore = useGenreStore()
-const movies = ref<Movie[]>(movieStore.newReleases)
-const genres = ref(genreStore.genres)
-const filteredMovies = ref<Movie[]>(movieStore.newReleases)
-const selectedGenre = ref<number | string>('')
+const newReleases = ref<Movie[]>([])
 const loading = ref(true)
 
-const currentPage = ref(1)
-const itemsPerPage = 40
-
-const totalPages = computed(() => Math.ceil(filteredMovies.value.length / itemsPerPage))
-
-const paginatedMovies = computed(() => {
-  const start = (currentPage.value - 1) * itemsPerPage
-  const end = start + itemsPerPage
-  return filteredMovies.value.slice(start, end)
-})
-
-const filterMovies = () => {
-  if (selectedGenre.value) {
-    filteredMovies.value = movieStore.getMoviesByGenre(Number(selectedGenre.value))
-  } else {
-    filteredMovies.value = movieStore.newReleases
-  }
-  currentPage.value = 1
-}
-
-const prevPage = () => {
-  if (currentPage.value > 1) {
-    currentPage.value--
-  }
-}
-
-const nextPage = () => {
-  if (currentPage.value < totalPages.value) {
-    currentPage.value++
-  }
-}
-
 onMounted(async () => {
-  await genreStore.fetchGenres()
-  await movieStore.fetchNewReleases(10) // Ajusta el número de páginas que deseas obtener para más variedad
-  genres.value = genreStore.genres
-  movies.value = movieStore.newReleases
-  filteredMovies.value = movieStore.newReleases
+  await movieStore.fetchNewReleases(1) // Obtén solo la primera página de las nuevas películas
+  newReleases.value = movieStore.newReleases.slice(0, 10) // Solo muestra las primeras 10 películas
   loading.value = false
 })
 </script>
@@ -92,28 +39,6 @@ onMounted(async () => {
 <style scoped>
 .new-releases {
   margin-top: 80px; /* Ajusta este valor si el navbar es más alto */
-  color: white;
-}
-
-.genre-select {
-  margin-bottom: 20px;
-}
-
-.genre-select label {
-  margin-right: 10px;
-  color: white;
-}
-
-.genre-select select {
-  padding: 5px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  background-color: #333;
-  color: white;
-}
-
-.genre-select select option {
-  background-color: #333;
   color: white;
 }
 
@@ -177,29 +102,5 @@ button:hover {
 
 h1 {
   color: white;
-}
-
-.pagination {
-  margin-top: 20px;
-  text-align: center;
-}
-
-.pagination button {
-  padding: 10px 15px;
-  margin: 0 5px;
-  border: none;
-  border-radius: 5px;
-  background-color: #444;
-  color: white;
-  cursor: pointer;
-}
-
-.pagination button:hover {
-  background-color: #555;
-}
-
-.pagination button:disabled {
-  background-color: #555;
-  cursor: not-allowed;
 }
 </style>
